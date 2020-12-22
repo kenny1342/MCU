475b3f22e28bb5c3107bf6ac783cc242c693f9ca
Ken-Roger Andersen <ken-roger.andersen@teleperformance.com>
2020-12-22 05:36:04 +0100

    env

---
+++ PumpHouseLogger/ESP8266-WiFi/env.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import os
+
+# Set environment variables
+os.environ['BLYNK_TOKEN'] = 'YvC4EfhIoGyWjgxEumhj-txdtwooSyg4'
+
+# export BLYNK_TOKEN='\"YvC4EfhIoGyWjgxEumhj-txdtwooSyg4\"'
