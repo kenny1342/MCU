ec74dcacfcaf921a4b87a89f07bce52f19183590
Ken-Roger Andersen <ken-roger.andersen@teleperformance.com>
2020-12-22 12:56:30 +0100

    Fixed lm335 readings. Added smoothing of ADC readings.

--- PumpHouseLogger/UNO-MCU/include/main.h
+++ PumpHouseLogger/UNO-MCU/include/main.h
@@ -2,18 +2,25 @@
 #ifndef __MAIN_H
 #define __MAIN_H
 
-#define FIRMWARE_VERSION            "0.14"
+#define FIRMWARE_VERSION            "0.15"
+#define numReadings                 10 // Define the number of samples to keep track of for ADC smoothing
 #define PRESSURE_SENS_MAX           10 // sensor maxmimum value (in Bar*1), currently using a 0-10Bar sensor
-#define CORR_FACTOR_PRESSURE_SENSOR 30  // correction factor (linear) (in Bar*100)
+#define CORR_FACTOR_PRESSURE_SENSOR 0.5  // correction factor (linear) (in Bar)
 #define ADC_CH_WATERP               1 // ADC connected to water pressure sensor
 #define ADC_CH_TEMP_1               0 // ADC connected to LM335 temp sensor 1
-#define ADC_SAMPLE_COUNT            5 // nr of ADC readings to calculate average from
+#define ADC_SAMPLE_COUNT            50 // nr of ADC readings to calculate average from
 
 typedef struct
 {
-  uint16_t adc_temp_1;
-  uint16_t adc_waterpump;
-  char temp_pumphouse[4];
+  uint16_t readings[numReadings];      // the readings from the analog input
+  uint8_t readIndex = 0;              // the index of the current reading
+  uint16_t total = 0;                  // the running total
+  uint16_t average = 0;                // the average
+} adc_avg;
+
+typedef struct
+{
+  char temp_pumphouse[5];
   char water_pressure_bar[4];
 } sensors_type;
 
