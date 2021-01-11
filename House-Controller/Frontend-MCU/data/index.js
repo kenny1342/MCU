596c756769b7f6d01edbcb7c986bfb2fcd635af2
Ken-Roger Andersen <ken-roger.andersen@teleperformance.com>
2021-01-11 03:34:05 +0100

    bugfix formatSecs

--- House-Controller/Frontend-MCU/data/index.js
+++ House-Controller/Frontend-MCU/data/index.js
@@ -1,20 +1,18 @@
-function formatSecs(secs=0) {
+function formatSecs(input=0) {
     
-    dateObj = new Date(secs * 1000); 
-    days = Math.round(secs/86400);
-    hours = dateObj.getUTCHours(); 
-    minutes = dateObj.getUTCMinutes(); 
-    seconds = dateObj.getSeconds(); 
+  let days = Math.floor(input / 86400);
+  let hours = Math.floor((input % 86400 ) / 3600) ;
+  let minutes = Math.floor(((input % 86400 ) % 3600 ) / 60 );
+  let seconds = ((input % 86400 ) % 3600 ) % 60  ;
   
-    timeString = days.toString() + ' days, ' +  
-    hours.toString().padStart(2, '0') 
-        + ':' + minutes.toString().padStart(2, '0') 
-        + ':' + seconds.toString().padStart(2, '0'); 
-        
-    return timeString;
+  timeString = days.toString() + ' days, ' +  
+  hours.toString().padStart(2, '0') 
+      + ':' + minutes.toString().padStart(2, '0') 
+      + ':' + seconds.toString().padStart(2, '0'); 
+    
+  return timeString;
   }
     
-  
   
   // Make sure JQuery script is loaded. Because of size, we try to include it from public/CDN first. If that fails, we load it locally (slower)
   
